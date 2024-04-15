c330f93c165cadde9d785289958fc4dd887a5c74
Preksha <prekshaparas1311@gmail.com>
2024-04-15 13:35:19 +0530

    api added [high vul]

--- src/app/components/global/Navbar.tsx
+++ src/app/components/global/Navbar.tsx
@@ -2,6 +2,8 @@ import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
 import { MenuIcon } from 'lucide-react'
+import { navigationMenuTriggerStyle } from "@/components/ui/navigation-menu"
+
 // import { UserButton, currentUser } from '@clerk/nextjs'
 
 type Props = {}
@@ -12,13 +14,7 @@ const Navbar = async (props: Props) => {
     <header className="fixed right-0 left-0 top-0 py-4 px-4 bg-black/40 backdrop-blur-lg z-[100] flex items-center border-b-[1px] border-neutral-900 justify-between">
       <aside className="flex items-center gap-[2px]">
         <p className="text-3xl font-bold">Fu</p>
-        <Image
-          src="/fuzzieLogo.png"
-          width={15}
-          height={15}
-          alt="fuzzie logo"
-          className="shadow-sm"
-        />
+        
         <p className="text-3xl font-bold">zie</p>
       </aside>
       <nav className="absolute left-[50%] top-[50%] transform translate-x-[-50%] translate-y-[-50%] hidden md:block">
@@ -27,19 +23,13 @@ const Navbar = async (props: Props) => {
             <Link href="#">Products</Link>
           </li>
           <li>
-            <Link href="#">Pricing</Link>
+            <Link href="#">Catalogues</Link>
           </li>
           <li>
-            <Link href="#">Clients</Link>
+            <Link href="#">Contact Us</Link>
           </li>
           <li>
-            <Link href="#">Resources</Link>
-          </li>
-          <li>
-            <Link href="#">Documentation</Link>
-          </li>
-          <li>
-            <Link href="#">Enterprise</Link>
+            <Link href="#">About Us</Link>
           </li>
         </ul>
       </nav>
