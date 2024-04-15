9b1744f40bfca4d06391e9939531dcb2124ca859
Preksha <prekshaparas1311@gmail.com>
2024-04-15 12:45:46 +0530

    navbar added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,10 @@
 import Image from "next/image";
+import Navbar from './components/global/Navbar';
 
 export default function Home() {
   return (
     <main >
-    hii
+    <Navbar/>
     </main>
   );
 }
